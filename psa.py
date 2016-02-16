554ea01a641146181fe24b222649e87631b56477
KTEQ DJ <julian.brackins@mines.sdsmt.edu>
2016-02-16 11:05:49 -0700

    updating PSA list 2-16-2016

--- psa.py
+++ psa.py
@@ -8,10 +8,17 @@ class PSA(object):
 
   def __init__(self):
     self.list = []
-    self.list.append("LISTEN UP MAGGOTS")
-    self.list.append("DAD O'CLOCK")
-    self.list.append("HEART ATTACK SONG")
-  
+    self.list.append("Adopt")
+    self.list.append("Best of Show")
+    self.list.append("Be Vocal")
+    self.list.append("Bullying")
+    self.list.append("Buzzed Driving")
+    self.list.append("Chance")    
+    self.list.append("Child Hunger")
+    self.list.append("Commune")
+    self.list.append("Funky Chicken ESP")
+    self.list.append("Stuntman")
+
 
 if __name__ == '__main__':
   print("Current PSAs in Rotation: ")
